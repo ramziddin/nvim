1931043ed3c328e295b85d0b668eb49ee173ae8d
Ramziddin Makhmudov <ramziddin.makhmudov@gmail.com>
2025-10-15 16:39:07 +0500

    init commit

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,22 @@
+return {
+  {
+    "catppuccin/nvim",
+    lazy = true,
+    opts = {
+      transparent_background = true,
+    },
+  },
+
+  {
+    "rose-pine/neovim",
+    lazy = true,
+    name = "rose-pine",
+  },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "rose-pine",
+    },
+  },
+}
