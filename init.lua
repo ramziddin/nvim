6837cf846288fbec5f82242da9a97762fb21bfdf
Ramziddin Makhmudov <ramziddin.makhmudov@gmail.com>
2025-12-18 11:42:52 +0500

    Add vtsls TypeScript LSP

--- init.lua
+++ init.lua
@@ -493,12 +493,7 @@ require("lazy").setup({
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
-        -- Some languages (like typescript) have entire language plugins that can be useful:
-        --    https://github.com/pmizio/typescript-tools.nvim
-        --
-        -- But for many setups, the LSP (`ts_ls`) will work just fine
-        -- ts_ls = {},
-        --
+        vtsls = {},
 
         lua_ls = {
           -- cmd = { ... },
